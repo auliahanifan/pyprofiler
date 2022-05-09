6c2cafa9607e8a8da224d6b82e78f2a102275965
auliahanifan <aulia.hanifan@bukuwarung.com>
2022-05-09 11:42:43 +0700

    chore: update url git on setup

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='auliarahman80@gmail.com',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    url='https://github.com/auliahanifan/pyprofiler',
+    url='https://github.com/auliahanifan/simpleprofiler.py',
     keywords='code profiler',
     install_requires=[
           'decorator',
